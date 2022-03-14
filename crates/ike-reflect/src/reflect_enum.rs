304f929e95ed21206021cdb2b8f0cb8c03b27192
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2022-03-14 05:08:54 +0100

    Make ReflectEnum return Variant

--- crates/ike-reflect/src/reflect_enum.rs
+++ crates/ike-reflect/src/reflect_enum.rs
@@ -1,7 +1,63 @@
-use crate::Reflect;
+use crate::{Reflect, ReflectStruct, ReflectTuple};
 
 pub trait ReflectEnum: Reflect {
     fn variant_name(&self) -> &str;
-    fn variant(&self) -> &dyn Reflect;
-    fn variant_mut(&mut self) -> &mut dyn Reflect;
+    fn variant_ref(&self) -> VariantRef;
+    fn variant_mut(&mut self) -> VariantMut;
+}
+
+pub enum VariantRef<'a> {
+    Tuple(&'a dyn ReflectTuple),
+    Struct(&'a dyn ReflectStruct),
+}
+
+pub enum VariantMut<'a> {
+    Tuple(&'a mut dyn ReflectTuple),
+    Struct(&'a mut dyn ReflectStruct),
+}
+
+impl<'a> VariantRef<'a> {
+    pub fn get_tuple(&self) -> Option<&'a dyn ReflectTuple> {
+        match *self {
+            Self::Tuple(reflect) => Some(reflect),
+            _ => None,
+        }
+    }
+
+    pub fn get_struct(&self) -> Option<&'a dyn ReflectStruct> {
+        match *self {
+            Self::Struct(reflect) => Some(reflect),
+            _ => None,
+        }
+    }
+}
+
+impl<'a> VariantMut<'a> {
+    pub fn get_tuple(&self) -> Option<&dyn ReflectTuple> {
+        match self {
+            Self::Tuple(reflect) => Some(*reflect),
+            _ => None,
+        }
+    }
+
+    pub fn get_struct(&self) -> Option<&dyn ReflectStruct> {
+        match self {
+            Self::Struct(reflect) => Some(*reflect),
+            _ => None,
+        }
+    }
+
+    pub fn get_tuple_mut(&mut self) -> Option<&mut dyn ReflectTuple> {
+        match self {
+            Self::Tuple(reflect) => Some(*reflect),
+            _ => None,
+        }
+    }
+
+    pub fn get_struct_mut(&mut self) -> Option<&mut dyn ReflectStruct> {
+        match self {
+            Self::Struct(reflect) => Some(*reflect),
+            _ => None,
+        }
+    }
 }
